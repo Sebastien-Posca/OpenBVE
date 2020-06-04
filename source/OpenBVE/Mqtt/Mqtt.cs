a445d46e51a14f55456c324f18b33f67c4fea213
Sebastien-Posca <sp06800@gmail.com>
2020-06-04 10:50:43 +0200

    Better control of power

--- source/OpenBVE/Mqtt/Mqtt.cs
+++ source/OpenBVE/Mqtt/Mqtt.cs
@@ -11,33 +11,53 @@ namespace OpenBve.Mqtt
 	public static class Mqtt
 	{
 		private static MqttClient client = new MqttClient("192.168.0.15");
-		//private static int cpt = 0;
 
 		static Mqtt()
 		{
 			client.MqttMsgPublishReceived += client_recievedMessage;
 			string clientId = Guid.NewGuid().ToString();
 			client.Connect(clientId);
-			client.Subscribe(new String[] { "/train/actuators/PowerUp" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+			client.Subscribe(new String[] { "/train/actuators/PowerUp", "/train/actuators/PowerDown" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
 
 		}
 		public static void Publish(String topic, String msg)
 		{
 			client.Publish(topic, Encoding.UTF8.GetBytes(msg), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
-
 		}
 
 		static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
 		{
-			if (!TrainManager.PlayerTrain.Handles.SingleHandle)
+			const string powerUp = "/train/actuators/PowerUp";
+			const string powerDown = "/train/actuators/PowerDown";
+
+			switch (e.Topic)
 			{
-				int p = TrainManager.PlayerTrain.Handles.Power.Driver;
-				if (p < TrainManager.PlayerTrain.Handles.Power.MaximumNotch)
-				{
-					TrainManager.PlayerTrain.ApplyNotch(1, true, 0, true);
-				}
+				case powerUp:
+					if (!TrainManager.PlayerTrain.Handles.SingleHandle)
+					{
+						int p = TrainManager.PlayerTrain.Handles.Power.Driver;
+						if (p < TrainManager.PlayerTrain.Handles.Power.MaximumNotch)
+						{
+							TrainManager.PlayerTrain.ApplyNotch(1, true, 0, true);
+						}
+					}
+					TrainManager.PlayerTrain.Handles.Power.ContinuousMovement = true;
+					break;
+				case powerDown:
+					if (!TrainManager.PlayerTrain.Handles.SingleHandle)
+					{
+						int p = TrainManager.PlayerTrain.Handles.Power.Driver;
+						if (p > 0)
+						{
+							TrainManager.PlayerTrain.ApplyNotch(-1, true, 0, true);
+						}
+					}
+					TrainManager.PlayerTrain.Handles.Power.ContinuousMovement = true;
+					break;
+				default:
+					break;
 			}
-			TrainManager.PlayerTrain.Handles.Power.ContinuousMovement = true;
 		}
+			
 	}
 }
